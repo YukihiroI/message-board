58d45182c3cf346f3a00c445e26298703d3b6d7b
yukihiro.iwata <yukihiro.iwata@rakuten.com>
2018-05-31 03:33:49 +0000

    CRUD messages

---
+++ resources/views/messages/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>メッセージ新規作成ページ</h1>
+
+    {!! Form::model($message, ['route' => 'messages.store']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('投稿') !!}
+
+    {!! Form::close() !!}
+
+    {{ '<p style="color: red;">When passed through htmlentities function</p>' }}
+    {!! '<p style="color: red;">When not passed through htmlentities function</p>' !!}
+
+@endsection
